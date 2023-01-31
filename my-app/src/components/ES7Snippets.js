9a1ef23b9d21824634d415c8e7b15b6d2feb391a
Alexa Hendry <alexahendry88@gmail.com>
2023-01-31 17:43:31 +0000

    Add ESJ7 Snippets extenstion and js file

---
+++ my-app/src/components/ES7Snippets.js
@@ -0,0 +1,50 @@
+import React from 'react'
+
+
+/* functional component */
+// function ES7Snippets() {
+//     return (
+//         <div>ES7Snippets</div>
+//     )
+// }
+
+// export default ES7Snippets
+
+
+/* functional component as arrow function */
+// export const ES7Snippets = () => {
+//     return (
+//         <div>ES7Snippets</div>
+//     )
+// }
+
+
+/* class component */
+// import React, { Component } from 'react'
+
+// export class ES7Snippets extends Component {
+//     render() {
+//         return (
+//         <div>ES7Snippets</div>
+//         )
+//     }
+// }
+
+// export default ES7Snippets
+
+
+/* constructor */
+// constructor(props) {
+//     super(props)
+
+//     this.state = {
+//         first
+//     }
+// }
+
+/* console log */
+// console.log(first)
+
+
+/* object console log */
+// console.log('first', first)
