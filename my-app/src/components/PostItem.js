2d7d6ded39cc47d14f6495b6d87d201a6f4f929f
Alexa Hendry <alexahendry88@gmail.com>
2023-02-03 17:35:48 +0000

    Destructured props on Content challenge

--- my-app/src/components/PostItem.js
+++ my-app/src/components/PostItem.js
@@ -5,14 +5,25 @@ export default function PostItem(props) {
 
     return (
             props.savedPosts.map(post => {
+                // desctrucured props version
+                const {name, title, description, image} = post;
                 return (
-                    <div key={post.title} className={css.SearchItem}>
-                        <p>{post.title}</p>
-                        <p>{post.name}</p>
-                        <img src={post.image} alt="" />
-                        <p>{post.description}</p>
+                    <div key={title} className={css.SearchItem}>
+                        <p>{title}</p>
+                        <p>{name}</p>
+                        <img src={image} alt="" />
+                        <p>{description}</p>
                     </div>
                 )
+                // non destructured version
+                // return (
+                //     <div key={post.title} className={css.SearchItem}>
+                //         <p>{post.title}</p>
+                //         <p>{post.name}</p>
+                //         <img src={post.image} alt="" />
+                //         <p>{post.description}</p>
+                //     </div>
+                // )
             })
     )
 }
